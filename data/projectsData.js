cfbcc2ff5c80869a2791913a5acce52ad4ac8c08
Tuong Pham Minh <77989401+TroyFPV48@users.noreply.github.com>
2024-07-03 23:45:20 -0700

    add nsc events project

--- data/projectsData.js
+++ data/projectsData.js
@@ -24,10 +24,19 @@ const projectsData = [
     thumbnail: "/projects/project3.jpg",
     description: "LG19 test for OED using Mocha and Chai",
     content:
-      "OED is The Open Energy Dashboard (OED) is a free, open source project that provides a web-based application to display energy information in a web browser and allows for the observation collection, graphing and conversions of various metrics and units of energy consumption. LG19 test is one of the many tests out there of OED purpose to ensure data unit and metric conversion is accurate, since some of them were created, our LG19 test would be similar and we just need to focus on the pattern and what makes LG19 test different.",
+      "OED is The Open Energy Dashboard (OED) is a free, open source project that provides a web-based application to display energy information in a web browser and allows for the observation collection, graphing and conversions of various metrics and units of energy consumption. \n\nLG19 test is one of the many tests out there of OED purpose to ensure data unit and metric conversion is accurate, since some of them were created, our LG19 test would be similar and we just need to focus on the pattern and what makes LG19 test different.",
     url: "https://www.linkedin.com/feed/update/urn:li:activity:7168880000018866176/",
   },
-  // Add more projects as needed
+  {
+    id: "nsc-practicum",
+    title: "North Seattle College Events",
+    thumbnail: "/projects/project4.jpg",
+    description:
+      "Seattle College web/app(s) help students navigate in the campus virtually.",
+    content:
+      "NSC Events: The NSC Events web and mobile applications allow students to view events hosted by the Student Leadership team and faculty at North Seattle College in a centralized place; Users can: Register an account, log into the app, view events & attend events; Admins can: Create event posts, edit event posts, archive event posts, delete event posts & edit users roles.",
+    url: "https://northseattlecollegeevents.com/",
+  },
 ];
 
 export default projectsData;
